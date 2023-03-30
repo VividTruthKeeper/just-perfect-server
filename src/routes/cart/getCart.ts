9be5ea453b9e4ed5a6eddfb893d78ee1ba3b0615
VividTruthKeeper <mr.aydogdy02@gmail.com>
2023-03-30 18:33:08 +0500

    cart get route

--- src/routes/cart/getCart.ts
+++ src/routes/cart/getCart.ts
@@ -1,7 +1,39 @@
 import express, { Request, Response } from "express";
-import { param } from "express-validator";
+import { body, param, validationResult } from "express-validator";
+import { GenericError } from "../../errors/generic-error";
+import { handleToken } from "../../functions/handleToken";
+import { Cart } from "../../models/cart.model";
+import { RequestValidationError } from "../../errors/request-validation-error";
 
 const router = express.Router();
 
-router.get("/api/cart", async (req: Request, res: Response) => {});
+router.get(
+  "/api/cart",
+  [
+    param("token").exists().withMessage("A valid token must be provided"),
+    body("userId")
+      .isLength({ min: 24, max: 24 })
+      .withMessage("A valid userId must be provided"),
+  ],
+  async (req: Request, res: Response) => {
+    const { token } = req.params;
+    const { userId } = req.body;
+    // req params error
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      throw new RequestValidationError(errors.array());
+    }
+
+    const decodedToken = await handleToken(token);
+
+    if (!decodedToken) throw new GenericError("Unhadled token error", 500);
+
+    const cart = await Cart.findOne({ user: userId }).populate("items.product");
+
+    res.status(200).send({
+      status: "success",
+      data: cart,
+    });
+  }
+);
 export { router as getCartRouter };
